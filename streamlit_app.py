6677166c75bccb097034631699e522050957b4e8
pravesh1693 <131159296+pravesh1693@users.noreply.github.com>
2023-04-21 20:06:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,9 +91,18 @@ streamlit.header("Fruityvice Fruit Advice!")
 # Snowflake Connector:
 import snowflake.connector
 
+#  Let's Query Our Trial Account Metadata:
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+# Let's Query Some Data, Instead:
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST;")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
