8ab4ebbbc35e922ac3f21b8f500c4e62e7329c7e
pravesh1693 <131159296+pravesh1693@users.noreply.github.com>
2023-04-24 17:32:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,7 +188,7 @@ streamlit.text(my_data_row)
 ## Oops! Let's Get All the Rows, Not Just One:
 # Move the Fruit Load List Query and Load into a Button Action:
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favourites!")
 
 # Snowflake related functions:
 
@@ -202,13 +202,14 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 ## Allow the end user to add a fruit to the list:
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values('from streamlit');")
+        my_cur.execute("insert into fruit_load_list values('"+ new_fruit +"');")
         return 'Thanks for adding ' + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
@@ -216,6 +217,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
 
     ## Add a STOP Command to Focus Our Attention:
