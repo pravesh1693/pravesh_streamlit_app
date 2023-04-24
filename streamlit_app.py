a54283aa3fd8c4e24773f08ffe0ba687ba40315a
pravesh1693 <131159296+pravesh1693@users.noreply.github.com>
2023-04-24 16:26:57 +0530

    Update streamlit_app.py
    adding a function.

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,6 +94,35 @@ streamlit.dataframe(fruits_to_show)
 ## Move the Fruityvice Code into a Try-Except (with a nested If-Else):
 # New Section to display Fruityvice api response:
 
+# streamlit.header("Fruityvice Fruit Advice!")
+
+# try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?') # Preloaded 'Kiwi' is removed.
+#   if not fruit_choice:
+#     streamlit.error('Please select a fruit to get information.')
+#   else:
+#       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#       streamlit.dataframe(fruityvice_normalized)
+
+# except URLError as e:
+#   streamlit.error()
+
+# Add a STOP Command to Focus Our Attention:
+# streamlit.stop()
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+
+## Move the Fruityvice Code into a Try-Except (with a nested If-Else):C
+# Create a Repeatable Code Block called a 'Function':
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
+
+# New Section to display Fruityvice api response:
+
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
@@ -101,9 +130,8 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
